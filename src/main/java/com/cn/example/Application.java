8ba9d9c256629b18c4a0bcf9535e9ce140da0965
375259457@qq.com <pyq152321>
2017-06-12 15:01:40 +0800

    first commit

---
+++ src/main/java/com/cn/example/Application.java
@@ -0,0 +1,14 @@
+package com.cn.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by YIYANG on 2017/6/12.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args){
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/com/cn/example/HelloWorld.java
@@ -0,0 +1,22 @@
+package com.cn.example;
+
+import javafx.application.Application;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by YIYANG on 2017/6/12.
+ */
+@RestController
+@EnableAutoConfiguration
+public class HelloWorld {
+    @RequestMapping("/")
+    public String list(){
+        return "Index page";
+    }
+
+}
+
+
