13368e1bdf30b41872dc01518f53925474fa3a01
375259457@qq.com <pyq152321>
2017-06-12 16:40:16 +0800

    puyq

--- src/main/java/com/cn/example/HelloWorld.java
+++ src/main/java/com/cn/example/HelloWorld.java
@@ -1,8 +1,9 @@
 package com.cn.example;
 
-import javafx.application.Application;
-import org.springframework.boot.SpringApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,9 +14,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @EnableAutoConfiguration
 public class HelloWorld {
+    protected static Logger logger = LoggerFactory.getLogger(HelloWorld.class);
     @RequestMapping("/")
     public String list(){
+        logger.info("访问hello list");
         return "Index page";
+    }
+    @RequestMapping("/hello/{name}")
+    public String helloName(@PathVariable String name){
+        logger.info("访问hello Name",name);
+        return "Hello "+name;
     }
 
 }
