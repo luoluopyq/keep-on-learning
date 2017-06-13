36827f9cfa269aba0cd843eeb1d34c9ab1881b7e
375259457@qq.com <pyq152321>
2017-06-13 09:46:46 +0800

    puyq通过@Configuration+@ComponentScan开启注解扫描并自动注册相应的注解Bean

--- src/main/java/com/cn/example/Application.java
+++ src/main/java/com/cn/example/Application.java
@@ -1,12 +1,17 @@
 package com.cn.example;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by YIYANG on 2017/6/12.
+ * 通过@Configuration+@ComponentScan开启注解扫描并自动注册相应的注解Bean
  */
-@SpringBootApplication
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 public class Application {
     public static void main(String[] args){
         SpringApplication.run(Application.class, args);
