d7cb688e6cd582c4bed6b029b34100cffe003460
luoluopyq@163.com <pyq152321>
2021-03-24 10:42:04 +0800

    单机手脚架项目

---
+++ scaffold/src/main/java/com/puyq/scaffold/ScaffoldApplication.java
@@ -0,0 +1,13 @@
+package com.puyq.scaffold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScaffoldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScaffoldApplication.class, args);
+    }
+
+}
---
+++ scaffold/src/test/java/com/puyq/scaffold/ScaffoldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.puyq.scaffold;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScaffoldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
